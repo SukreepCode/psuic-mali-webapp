61420842134c1fdd949e9f20cd4dc90eb879ce5f
Thada Wangthammang <mildronize@gmail.com>
2020-10-27 14:21:41 +0700

    add basic column entity

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,1 +1,21 @@
-export class UsersEntity {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class UserEntity {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  displayID: string;
+
+  @Column({ length: 255 })
+  name: string;
+
+  @Column({ length: 255 })
+  email: string;
+
+  @Column({ length: 255 })
+  password: string;
+
+}
