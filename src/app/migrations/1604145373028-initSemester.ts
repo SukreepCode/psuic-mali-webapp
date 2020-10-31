9c5dffc4e1f86f0847233a277c73ae52f9cf3987
Thada Wangthammang <mildronize@gmail.com>
2020-10-31 19:01:41 +0700

    add seed for semesters ( migration)

---
+++ src/app/migrations/1604145373028-initSemester.ts
@@ -0,0 +1,15 @@
+import { getRepository, MigrationInterface, QueryRunner } from 'typeorm';
+import { SemestersSeed } from '../seeds/semesters.seed';
+import { SemestersEntity } from '../../semesters/semesters.entity';
+
+
+export class initSemester1604145373028 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await getRepository(SemestersEntity).save(SemestersSeed);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
---
+++ src/app/seeds/semesters.seed.ts
@@ -0,0 +1,5 @@
+export const SemestersSeed = [
+  {
+    id: '2020/2',
+  }
+];
