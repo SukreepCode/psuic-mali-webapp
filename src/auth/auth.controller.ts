270395e3f6d60b87368d09b20c5377605596de69
Thada Wangthammang <mildronize@gmail.com>
2020-10-31 23:47:02 +0700

    add local passport

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,23 @@
 import {
-  Controller, Post, Body, HttpException, HttpStatus, Logger,
+  Controller, Post, Body, HttpException, HttpStatus, Logger, UseGuards, Request
 } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { CreateUserDto } from '../users/users.dto';
 import { assignObject } from '../app/utils';
 import { UsersEntity } from '../users/users.entity';
 
+import { AuthGuard } from '@nestjs/passport';
+// import {LocalAuthGuard} from './local-auth.guard';
+
 @Controller('auth')
 export class AuthController {
   constructor(private readonly usersService: UsersService) {}
+
+  @UseGuards(AuthGuard('local'))
+  @Post('/login')
+  async login(@Request() req) {
+    return req.user;
+  }
 
   @Post('/signup')
   public async signUp(@Body() newUser: CreateUserDto) {
@@ -27,4 +36,5 @@ export class AuthController {
     const user: UsersEntity = assignObject(new UsersEntity(), createUser);
     return await this.usersService.createOrUpdate(user);
   }
+
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,10 +5,12 @@ import { UsersService } from '../users/users.service';
 
 import { UsersEntity } from '../users/users.entity';
 import { UsersModule } from '../users/users.module';
+import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
   imports: [UsersModule, TypeOrmModule.forFeature([UsersEntity])],
   controllers: [AuthController],
-  providers: [UsersService],
+  providers: [UsersService, AuthService, LocalStrategy],
 })
 export class AuthModule {}
