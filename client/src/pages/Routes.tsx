24523192109db3ae0e372ec4a1d7a38f407035f1
Thada Wangthammang <mildronize@gmail.com>
2020-11-14 20:09:03 +0700

    fix all route

--- client/src/pages/Routes.tsx
+++ client/src/pages/Routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route , Redirect} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 import PrivateRoute from '../components/PrivateRoute';
 
@@ -8,6 +8,7 @@ import Login from './Login';
 import Evaluation from './Evaluation';
 import Logout from './Logout';
 import ValidatingToken from '../components/ValidatingToken';
+import Page404 from './Page404';
 
 export const LOGIN_PATH = '/login';
 export const VALIDATING_TOKEN_PATH = '/validating-token';
@@ -21,17 +22,17 @@ const Routes: React.FunctionComponent = () => {
         <Route exact path={LOGIN_PATH} >
           <Login loginSuccessRoute="/evaluation" />
         </Route>
-      
+
         <Route path="/logout" component={Logout} />
 
         <PrivateRoute exact path="/evaluation" component={Evaluation} />
 
-      {/* Catch all route */}
-        {/* <Route render={() => <Redirect to={LOGIN_PATH} />} /> */}
+        <Route path={VALIDATING_TOKEN_PATH} component={ValidatingToken} />
 
-        <Route exact path={VALIDATING_TOKEN_PATH} component={ValidatingToken} />
+        {/* Catch all route */}
+        <Route component={Page404} />
       </Switch>
-      
+
     </Router>
   );
 }
